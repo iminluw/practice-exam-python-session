9c59767cafdcfe2db09e75aa5faf29e9e150e8f2
iminluw <148132606+iminluw@users.noreply.github.com>
2026-05-11 11:33:36 +0900

    Update user.py

--- models/user.py
+++ models/user.py
@@ -1,17 +1,24 @@
 from datetime import datetime
 
 class User:
-    def __init__(self, username, email, role, id=None):
+    def __init__(self, username, email, role, id=None, password=None):
         self.id = id
         self.username = username
         self.email = email
         self.role = role
+        self.password = password
         self.registration_date = datetime.now()
 
-    def update_info(self, username=None, email=None, role=None):
-        if username: self.username = username
-        if email: self.email = email
-        if role: self.role = role
+    @staticmethod
+    def from_tuple(data):
+        if not data: return None
+        # Сопоставляем поля из БД: id, username, email, password, role
+        return User(id=data[0], username=data[1], email=data[2], password=data[3], role=data[4])
 
     def to_dict(self):
-        return self.__dict__
+        return {
+            "id": self.id,
+            "username": self.username,
+            "email": self.email,
+            "role": self.role
+        }
